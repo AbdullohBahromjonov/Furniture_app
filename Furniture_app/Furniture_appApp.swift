bbee6f58e59022be9e7a43b35a00b2a279d24979
Abdulloh <abdullohbahromjonv@gmail.com>
2022-07-29 20:48:35 +0500

    Initial Commit

---
+++ Furniture_app/Furniture_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Furniture_appApp.swift
+//  Furniture_app
+//
+//  Created by Zafarjon on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Furniture_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Furniture_appUITests/Furniture_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Furniture_appUITestsLaunchTests.swift
+//  Furniture_appUITests
+//
+//  Created by Zafarjon on 29/07/22.
+//
+
+import XCTest
+
+class Furniture_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
